26c34051b1487245f5f8c2e39d3ba5ca42beac24
Aleš Vaupotič <ales@vaupotic.eu>
2023-08-25 22:14:33 +0200

    reexport

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,11 +1,37 @@
 // Reexport your entry components here
-export { BrowserData, CardWrapper, ItemsCenter, JSON, RefreshAlert, TailwindSize } from "./general";
+
+import BrowserData from "./general/BrowserData.svelte";
+import CardWrapper from "./general/CardWrapper.svelte";
+import ItemsCenter from "./general/ItemsCenter.svelte";
+import JSON from "./general/JSON.svelte";
+import RefreshAlert from "./general/RefreshAlert.svelte";
+import TailwindSize from "./general/TailwindSize.svelte";
+
+import ButtonBack from "./navigation/ButtonBack.svelte";
+import ButtonNew from "./navigation/ButtonNew.svelte";
+import DesktopNav from "./navigation/DesktopNav.svelte";
+import MenuOptions from "./navigation/MenuOptions.svelte";
+import NavOptions from "./navigation/NavOptions.svelte";
+import Navigation from "./navigation/Navigation.svelte";
+import StickySeparator from "./navigation/StickySeparator.svelte";
+import UserData from "./navigation/UserData.svelte";
+
+export {
+    BrowserData,
+    CardWrapper,
+    ItemsCenter,
+    JSON,
+    RefreshAlert,
+    TailwindSize
+}
 
 export {
     ButtonBack,
     ButtonNew,
-    DesktopNav, MenuOptions, Navigation,
+    DesktopNav,
+    MenuOptions,
+    Navigation,
     NavOptions,
     StickySeparator,
     UserData
-} from "./navigation";
+} 
